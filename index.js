9b44297aea063aeb2181a29a13a03feae5b95bd9
sonu kumar <96580671+ysonu1071@users.noreply.github.com>
2022-11-01 11:57:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var menu = document.querySelector(".menu_btn");
+var navlist = document.querySelector(".nave_container");
+function change() {
+    menu.classList.toggle("active");
+    navlist.classList.toggle("active");
+}
+
